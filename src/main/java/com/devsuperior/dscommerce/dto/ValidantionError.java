a2f541cf7fb12cfdf9d319561e66bfe01a16fb67
Felipe Lustosa <lustosaf590@gmail.com>
2025-02-08 00:07:13 -0300

    Fix Validation

--- src/main/java/com/devsuperior/dscommerce/dto/ValidantionError.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidantionError.java
@@ -16,7 +16,11 @@ public class ValidantionError extends CustomError{
         return errors;
     }
 
+    // ser eu chamar esse metado mais de uma vez com o mesmo nome do campo ele vai adiciona mais de um erro para essse campo
+    // no caso eu quero q cada campo da entidade tenha apenas um erro, no front eu vou trata um erro de cada vez
     public  void addError(String fieldName,String message){
+        // Eu vou remover um possivel msg com esse mesmo nome que ja exista e vou adiciona uma nova msg
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
         errors.add(new FieldMessage(fieldName,message));
     }
 }
